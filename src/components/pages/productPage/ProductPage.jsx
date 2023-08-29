72460fb836330abbf92c3e6e9a2a845a7333c845
F3riko <vyrydy@mail.ru>
2023-08-29 14:34:22 +0300

    Advanced product page
    1. Responsive page design
    2. Carousel for imgs
    3. Description and specs blocks
    4. Basic specs rendering
    5. New cart and price block

--- src/components/pages/productPage/ProductPage.jsx
+++ src/components/pages/productPage/ProductPage.jsx
@@ -4,10 +4,11 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
+import Carousel from "react-bootstrap/Carousel";
+import CartBlock from "./productPageCartBlock/CartBlock";
 
 // Dummy data
 import dummyData from "../../dummydata/electronicsData";
-import CartBlock from "./productPageCartBlock/CartBlock";
 
 const ProductPage = () => {
   const { productId } = useParams();
@@ -16,26 +17,68 @@ const ProductPage = () => {
 
   useEffect(() => {
     setProduct(dummyData[productId]);
+    console.log(product);
   }, [productId]);
 
   return (
-    <Container fluid>
+    <Container>
       <Row>
-        <h3 className="text-center">{product.name}</h3>
+        <h4 className="text-center mt-3">{product.name}</h4>
       </Row>
       <Row>
-        <Col xs={12} md={4}>
-          <Image src="/placeholder1.png" className="product-tile-thubmnail" />
+        <Col xs={12} md={3} className="pr-page-gallery-wrapper">
+          <Carousel fade variant="dark">
+            <Carousel.Item className="text-center">
+              <Image src="/placeholder1.png" className="pr-page-main-image" />
+            </Carousel.Item>
+            <Carousel.Item className="text-center">
+              <Image tesrc="/placeholder1.png" className="pr-page-main-image" />
+            </Carousel.Item>
+            <Carousel.Item className="text-center">
+              <Image src="/placeholder1.png" className="pr-page-main-image" />
+            </Carousel.Item>
+          </Carousel>
         </Col>
-        <Col xs={12} md={4}>
+        <Col xs={12} md={5} className="pr-page-short-specs d-flex flex-column">
           {/* <span>Category: {categories[product.category].category_name}</span> */}
-          <span>Description: {product.short_description}</span>
           <span>Year of production: {product.year_of_production}</span>
           <span>Weight: {product.weight}g</span>
           <span>In stock: {product.stock_quantity}</span>
+          <a href="#description">View details</a>
         </Col>
         <Col xs={12} md={4}>
           <CartBlock price={product.price} discount={"200"} />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} md={8} className="px-0">
+          <div className="pr-page-description mt-3">
+            <h5 className="text-center">About this product</h5>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel
+              consequat purus. Vivamus nec pulvinar risus. Donec nisl risus,
+              euismod quis posuere et, ullamcorper vitae eros. Aenean ultrices
+              mi id dolor porta, sit amet dictum tortor facilisis. Maecenas erat
+              magna, venenatis eu aliquam ac, blandit quis odio. Mauris vitae
+              dignissim urna. Ut at vulputate ipsum. Donec ornare consectetur
+              interdum. Suspendisse potenti. Sed libero sem, euismod non tellus
+              in, egestas tempor urna. Phasellus finibus molestie elit, vel
+              accumsan nulla convallis id. Nunc gravida magna quam, ac dictum
+              elit sagittis vitae. Aliquam mi urna, sagittis nec rutrum et,
+              gravida eu tellus. Sed bibendum dolor placerat quam luctus
+              scelerisque. Donec lorem nulla, finibus feugiat elit ac, ultrices
+              scelerisque orci. Donec ac nisl nisi.
+            </p>
+          </div>
+          <div className="pr-page-full-details my-2" id="description">
+            <h5 className="text-center">Detailed product info</h5>
+            <p>
+              category: Lorem ipsum dolor sit amet, consectetur adipisicing
+              elit. Praesentium sapiente delectus aspernatur rem officia. Modi
+              cupiditate magni ducimus fugit quas impedit, sed doloribus
+              molestiae nam.
+            </p>
+          </div>
         </Col>
       </Row>
     </Container>
--- src/components/pages/productPage/productPageCartBlock/CartBlock.jsx
+++ src/components/pages/productPage/productPageCartBlock/CartBlock.jsx
@@ -2,32 +2,70 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import Counter from "../../../supportComponents/Counter";
+import PriceBlock from "../../../supportComponents/PriceBlock";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as faHeartSolid } from "@fortawesome/free-solid-svg-icons";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
 
 const CartBlock = ({ price, discount }) => {
-  const Price = () => {
-    return (
-      <>
-        <Col md={discount ? 6 : 12} className="d-flex justify-content-center">
-          <p className="product-page-cart-block-price">{price}</p>
-        </Col>
-        {discount && (
-          <Col md={6} className="d-flex justify-content-center">
-            <p className="product-page-cart-block-discount">discount</p>
-          </Col>
-        )}
-      </>
-    );
+  const [cartQuantity, setCartQuantity] = useState(0);
+  const [liked, setLiked] = useState(false);
+
+  const handleCart = (action) => {
+    if (action === "+") {
+      setCartQuantity((prevValue) => prevValue + 1);
+    } else {
+      setCartQuantity((prevValue) => prevValue - 1);
+    }
   };
+
+  const handleLike = () => {
+    setLiked((prevValue) => !prevValue);
+  };
+
   return (
     <Container className="product-page-cart-block-container">
       <Row>
-        <Price />
+        <Col className="d-flex justify-content-between align-items-center mb-2">
+          <PriceBlock discount={discount} price={price} />
+          {liked ? (
+            <FontAwesomeIcon
+              icon={faHeartSolid}
+              style={{ color: "#f8104b" }}
+              size="lg"
+              onClick={handleLike}
+              className="me-1"
+            />
+          ) : (
+            <FontAwesomeIcon
+              icon={faHeart}
+              onClick={handleLike}
+              size="lg"
+              className="me-1"
+            />
+          )}
+        </Col>
       </Row>
       <Row>
-        <Button variant="success">{discount}</Button>
-      </Row>
-      <Row className="mt-2">
-        <Button variant="danger">Wishlist</Button>
+        <Col>
+          {cartQuantity === 0 ? (
+            <Button
+              variant="primary"
+              className="mb-2 w-100"
+              onClick={() => {
+                handleCart("+");
+              }}
+            >
+              <FontAwesomeIcon icon={faCartShopping} />
+              <span className="cart-text ms-1">Cart</span>
+            </Button>
+          ) : (
+            <Counter handleCounter={handleCart} cartState={cartQuantity} />
+          )}
+        </Col>
       </Row>
     </Container>
   );
