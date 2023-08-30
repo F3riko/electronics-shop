050ade52802da2929e39131c7f5abed4b3fb1306
F3riko <vyrydy@mail.ru>
2023-08-30 19:07:14 +0300

    Custom hook for data fetching
    useFetch func and two implementations of it with
    corresponding chagnes

--- src/components/pages/cart/ProductPreviewCardCart.jsx
+++ src/components/pages/cart/ProductPreviewCardCart.jsx
@@ -13,21 +13,16 @@ import { useState, useEffect } from "react";
 import PriceBlock from "../../supportComponents/PriceBlock";
 // import { getCategoryNameById } from "../../../utils/categoryUtils";
 import { faHeart as faHeartSolid } from "@fortawesome/free-solid-svg-icons";
+import useFetch from "../../../utils/useFetch";
+import { getProduct } from "../../../services/api/getProduct-api";
 
-const ProductPreviewCardCart = ({ productData, selected, handleSelect }) => {
+const ProductPreviewCardCart = ({ selected, handleSelect, itemId }) => {
   const [isSmallScreen, setIsSmallScreen] = useState(false);
   const [liked, setLiked] = useState(false);
+  const { data, loading, error } = useFetch(getProduct, [itemId]);
+  const { cart, handleCart } = useAuth();
   // const categories = useContext(HomeContext);
   // const categoryName = getCategoryNameById(categories, productData.category_id);
-  const price = parseInt(productData.price);
-  // Testing for discount
-  // const discount =
-  //   (productData.discount && parseInt(productData.discount)) || null;
-  const discount = 25;
-
-  const handleLike = () => {
-    setLiked((prevValue) => !prevValue);
-  };
 
   useEffect(() => {
     const handleResize = () => {
@@ -40,93 +35,105 @@ const ProductPreviewCardCart = ({ productData, selected, handleSelect }) => {
     };
   }, []);
 
+  const handleLike = () => {
+    setLiked((prevValue) => !prevValue);
+  };
+
   const ProductTitle = () => {
     return (
       <Row className="mb-2 cart-product-title-text">
-        <CustomLink to={`/product/${productData.id}`}>
-          <Col>{productData.title}</Col>
+        <CustomLink to={`/product/${data.id}`}>
+          <Col>{data.title}</Col>
         </CustomLink>
       </Row>
     );
   };
-  const { cart, handleCart } = useAuth();
+
   return (
     <Container fluid className="cart-pr-tile-wrapper">
-      <Row>
-        <Col
-          md={1}
-          xs={1}
-          className="d-flex flex-column align-items-center justify-content-center px-0"
-        >
-          <Form.Check type="checkbox" id={productData.id}>
-            <Form.Check.Input
-              type="checkbox"
-              className="cart-custom-check-box"
-              checked={selected}
-              onChange={() => handleSelect(productData.id)}
-            />
-          </Form.Check>
-        </Col>
-
-        <Col xs={12} md={3} className="cart-product-tile-thubmnail-wrapper">
-          <Link to={`product/${productData.id}`}>
-            <Image
-              src="/placeholder1.png"
-              className="cart-product-tile-thubmnail"
-            />
-          </Link>
-        </Col>
-
-        <Col className="mt-1 cart-middle-column">
-          <ProductTitle />
-          <span>
-            <span className="cart-secondary-text">Category</span>:{" "}
-            {/* {categoryName} */}
-          </span>
-          {/* No description here, only list of specs from db */}
-
-          {/* <span>Description: {productData.description}</span> */}
-          {/* <span>Year of production: {productData.year_of_production}</span> */}
-          {/* <span>Weight: {productData.weight}g</span> */}
-          {/* <span>In stock: {productData.stock_quantity}</span> */}
-        </Col>
-
-        {isSmallScreen && <ProductTitle />}
-
-        <Col
-          xs={12}
-          md={4}
-          className="d-flex flex-column justify-content-between"
-        >
+      {loading && <h5>Loading...</h5>}
+      {data && (
+        <>
           <Row>
-            <Col className="px-4 d-flex justify-content-between align-items-center">
-              <PriceBlock price={price} discount={discount} />
-              {liked ? (
-                <FontAwesomeIcon
-                  icon={faHeartSolid}
-                  style={{ color: "#f8104b" }}
-                  size="lg"
-                  onClick={handleLike}
-                  className="me-1"
+            <Col
+              md={1}
+              xs={1}
+              className="d-flex flex-column align-items-center justify-content-center px-0"
+            >
+              <Form.Check type="checkbox" id={data.id}>
+                <Form.Check.Input
+                  type="checkbox"
+                  className="cart-custom-check-box"
+                  checked={selected}
+                  onChange={() => handleSelect(data.id)}
                 />
-              ) : (
-                <FontAwesomeIcon
-                  icon={faHeart}
-                  onClick={handleLike}
-                  size="lg"
-                  className="me-1"
+              </Form.Check>
+            </Col>
+
+            <Col xs={12} md={3} className="cart-product-tile-thubmnail-wrapper">
+              <Link to={`/product/${data.id}`}>
+                <Image
+                  src="/placeholder1.png"
+                  className="cart-product-tile-thubmnail"
                 />
-              )}
+              </Link>
+            </Col>
+
+            <Col className="mt-1 cart-middle-column">
+              <ProductTitle />
+              <span>
+                <span className="cart-secondary-text">Category</span>:{" "}
+                {/* {categoryName} */}
+              </span>
+              {/* No description here, only list of specs from db */}
+
+              {/* <span>Description: {productData.description}</span> */}
+              {/* <span>Year of production: {productData.year_of_production}</span> */}
+              {/* <span>Weight: {productData.weight}g</span> */}
+              {/* <span>In stock: {productData.stock_quantity}</span> */}
+            </Col>
+
+            {isSmallScreen && <ProductTitle />}
+
+            <Col
+              xs={12}
+              md={4}
+              className="d-flex flex-column justify-content-between"
+            >
+              <Row>
+                <Col className="px-4 d-flex justify-content-between align-items-center">
+                  <PriceBlock
+                    price={data.price * cart.items[itemId].quantity}
+                    discount={data.discount * cart.items[itemId].quantity}
+                  />
+                  {liked ? (
+                    <FontAwesomeIcon
+                      icon={faHeartSolid}
+                      style={{ color: "#f8104b" }}
+                      size="lg"
+                      onClick={handleLike}
+                      className="me-1"
+                    />
+                  ) : (
+                    <FontAwesomeIcon
+                      icon={faHeart}
+                      onClick={handleLike}
+                      size="lg"
+                      className="me-1"
+                    />
+                  )}
+                </Col>
+              </Row>
+
+              <Counter
+                handleCounter={handleCart}
+                cartState={cart.items[itemId].quantity}
+                itemId={data.id}
+              />
             </Col>
           </Row>
-
-          <Counter
-            handleCounter={handleCart}
-            cartState={cart.items[productData.id].quantity}
-            itemId={productData.id}
-          />
-        </Col>
-      </Row>
+        </>
+      )}
     </Container>
   );
 };
