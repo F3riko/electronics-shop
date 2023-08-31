1924f0e60aaf4e6306db3ed6a7416ec2fb9cc9f5
F3riko <vyrydy@mail.ru>
2023-08-31 17:54:45 +0300

    Basic footer

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -17,6 +17,7 @@ import CustomLink from "./supportComponents/CustomLink";
 import ForgotPassword from "./sign-up-login/ForgotPassword";
 import { useAuth } from "./supportComponents/AuthProvider";
 import { logOutUser } from "../services/api/logOut-api";
+import Footer from "./Footer";
 
 const NavBar = () => {
   const [showModal, setShowLogin] = useState({
@@ -143,6 +144,7 @@ const NavBar = () => {
         }}
       />
       <Outlet />
+      <Footer />
     </>
   );
 };
